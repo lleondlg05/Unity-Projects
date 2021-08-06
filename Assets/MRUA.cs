66e104b0436465dd19bd878ba15c3a1d60094ea9
lleondlg05 <luisleon05.lcld@gmail.com>
2021-08-06 23:09:23 +0200

    Parabolic

--- Assets/MRUA.cs
+++ Assets/MRUA.cs
@@ -11,26 +11,42 @@ public class MRUA : MonoBehaviour
     public Direction movementDirection;
     private float totalMovingSeconds;
 
-    void Start()
-    {
-        transform.position = new Vector2(initPosition, 0);
-    }
-
     void Update()
     {
+        Vector2 direction = Vector2.right;
+        if(Direction.Right == movementDirection)
+        {
+            direction = Vector2.right;
+        }
+        else
+        {
+            direction = Vector2.up;
+        }
+        
         totalMovingSeconds += Time.deltaTime;
 
         if (totalMovingSeconds <= maxMovementSeconds)
         {
-            transform.position = Vector2.right * (initPosition + initVelocity * totalMovingSeconds + 0.5f * aceleration * totalMovingSeconds * totalMovingSeconds);
+            Vector3 currentPosition = transform.position;
+            if(Direction.Right == movementDirection)
+            {
+                currentPosition.x = (initPosition + initVelocity * totalMovingSeconds + 0.5f * aceleration * totalMovingSeconds * totalMovingSeconds);
+            }
+            else
+            {
+                currentPosition.y = (initPosition + initVelocity * totalMovingSeconds + 0.5f * aceleration * totalMovingSeconds * totalMovingSeconds);
+            }
+            transform.position = currentPosition;
 
             //transform.position = 
             //      Vector2.right * initPosition 
             //    + Vector2.right * velocity * totalMovingSeconds 
             //    + Vector2.right * 0.5f * aceleration * totalMovingSeconds * totalMovingSeconds;
-            
+
 
         }
+        else
+            totalMovingSeconds = 0;
     }
 
     public enum Direction
